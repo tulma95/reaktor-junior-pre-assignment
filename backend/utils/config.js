a9a4db7e1b536823dfc7e398a891f56c1a13a614
Riku Honkanen <riku.honkanen9595@gmail.com>
2019-12-31 00:56:10 +0200

    Add heroku path to status

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -9,7 +9,9 @@ if (process.env.NODE_ENV !== 'production') {
 let PATH = fs.existsSync('/var/lib/dpkg/status') ?
   '/var/lib/dpkg/status' : path.resolve(__dirname, `../statustest.txt`)
 
-
+if (process.env.HEROKU === 1) {
+  PATH = path.resolve(__dirname, `../statustest.txt`
+}
 
 
 let PORT = process.env.PORT
