08f3daf4b9022d9c0c535cb984a39b2b301464a4
Riku Honkanen <riku.honkanen9595@gmail.com>
2020-01-30 14:44:21 +0200

    refactor

--- react-frontend/src/components/SinglePackage.js
+++ react-frontend/src/components/SinglePackage.js
@@ -1,8 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Link,
-  useParams
-} from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 const SinglePackage = ({ packages, setMessage }) => {
   const [pack, setPack] = useState()
@@ -11,11 +8,10 @@ const SinglePackage = ({ packages, setMessage }) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true)
       try {
         const res = await fetch(`/api/${name}`, {
           headers: {
-            "accepts": "application/json"
+            accepts: 'application/json'
           }
         })
         const data = await res.json()
@@ -23,73 +19,79 @@ const SinglePackage = ({ packages, setMessage }) => {
         setLoading(false)
         setMessage(null)
       } catch (error) {
-        setMessage(`No data for package ${name} found!!`)
+        setMessage(`No data for package ${name} found!`)
       }
     }
     fetchData()
   }, [name, setMessage])
 
-  const parseAlternates = (dependency) => {
-    const dependencies = dependency.split('|')
-      .map(e => e.trim())
+  if (loading) return <div>Loading...</div>
+
+  const parseAlternates = dependency => {
+    const dependencies = dependency.split('|').map(e => e.trim())
 
     const found = packages.find(pack => dependencies.includes(pack.name))
 
-    if (!found) return <div key={dependency}>{dependency}</div>
+    if (!found)
+      return (
+        <div className='GridItem' key={dependency}>
+          {dependency}
+        </div>
+      )
 
     return (
-      <div className='GridItem'>
-        <Link to={`${found.name}`}>
-          {found.name}
-        </Link>
+      <div key={dependency} className='GridItem'>
+        <Link to={`${found.name}`}>{found.name}</Link>
         {dependencies
           .filter(dependency => dependency !== found.name)
-          .map((dependency, i) => <span key={i}> | {dependency}</span>)}
-      </div>
-    )
-  }
-
-
-  if (loading) {
-    return <div>Loading...</div>
-  } else {
-    return (
-      <div className='SinglePackage'>
-        <h1>{pack.name}</h1>
-
-        <h2>Description</h2>
-        <div>{pack.description
-          .replace(/^ \./gm, ' ')
-          .split('\n')
-          .map((line, i) => <pre key={i}>{line}</pre>)}
-        </div>
-
-        <h2>Dependencies</h2>
-        <div className='Grid'>
-          {pack.dependencies
-            .map((dependency, i) => {
-              if (dependency.includes('|')) {
-                return parseAlternates(dependency)
-              }
-              return (
-                <Link className='GridItem' key={i} to={`${dependency}`}>
-                  <div>{dependency}</div>
-                </Link>
-              )
-            })}
-        </div>
-
-        <h2>Dependants</h2>
-        <div className='Grid'>
-          {pack.dependants.map((dependant, i) => (
-            <Link className='GridItem' key={i} to={`${dependant}`}>
-              <div >{dependant}</div>
-            </Link>
+          .map((dependency, i) => (
+            <span key={i}> | {dependency}</span>
           ))}
-        </div>
       </div>
     )
   }
+
+  const description = description => description.replace(/^ \./gm, ' ')
+
+  return (
+    <div className='SinglePackage'>
+      <h1>{pack.name}</h1>
+
+      <h2>Description</h2>
+      <div>{<pre>{description(pack.description)}</pre>}</div>
+
+      <h2>Dependencies</h2>
+      <div className='Grid'>
+        <Dependencies
+          dependencies={pack.dependencies}
+          parseAlternates={parseAlternates}
+        />
+      </div>
+
+      <h2>Dependants</h2>
+      <div className='Grid'>
+        {pack.dependants.map((dependant, i) => (
+          <Link className='GridItem' key={i} to={`${dependant}`}>
+            <div>{dependant}</div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const Dependencies = ({ dependencies, parseAlternates }) => {
+  console.log(dependencies)
+  return dependencies.map((dependency, i) => {
+    if (dependency.includes('|')) {
+      return parseAlternates(dependency)
+    }
+    return (
+      <Link className='GridItem' key={i} to={`${dependency}`}>
+        <div>{dependency}</div>
+      </Link>
+    )
+  })
 }
 
 export default SinglePackage
---
+++ react-frontend/src/components/presentationals/Package.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Package = ({ name, description, dependencies, dependants }) => {
+  return <div></div>
+}
+
+export default Package
